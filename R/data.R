906dce78c37413c71bbb4eb5cc409503673d6a82
Barnaby Sanderson <barnaby.sanderson@gstt.nhs.uk>
2018-09-05 17:00:14 +0100

    added DOuglas table 4 data

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' 38 arterial and venous blood gas measurements.
+#' Table 3 - 38 arterial and venous blood gas measurements.
 #'
 #' A dataset containing 38 venous and arterial blood gas measurents
 #' from five patients. Presented as table 3 in \insertCite{douglas_1988}{co2ntent}
@@ -21,3 +21,27 @@
 #'   \insertRef{loeppky_1983}{co2ntent}
 #' }
 "douglas_table_3"
+
+#' Table 4 - 46 arterial blood gas measurements.
+#'
+#' A dataset containing 46 arterial blood gas measurents
+#' from four patients. Presented as table 4 in \insertCite{douglas_1988}{co2ntent}
+#' from the work of \insertCite{jones_1970}{co2ntent}
+#'
+#' @format A data frame with 48 rows and 8 variables:
+#' \describe{
+#'   \item{subject}{subject, of four participants}
+#'   \item{po2_torr}{partial pressure of O2 in torr(mmHg), contains some non numeric entries!}
+#'   \item{ph}{ph, hydrogen ion concentration}
+#'   \item{haemoglobin_g_dl}{haemoglobin, in grams per decilitre}
+#'   \item{so2_fraction}{haemoglobin oxygen saturation as a fraction, ie between 0 and 1}
+#'   \item{blood_co2_content_ml_dl}{CO2 content of blood in mls per decilitre}
+#'   \item{pco2_torr}{partial pressure of CO2 in torr(mmHg)}
+#'   \item{plasma_co2_content_ml_dl}{CO2 content of plasma in mls per decilitre}
+#' }
+#'
+#' @references{
+#'   \insertRef{douglas_1988}{co2ntent}
+#'   \insertRef{jones_1970}{co2ntent}
+#' }
+"douglas_table_4"
